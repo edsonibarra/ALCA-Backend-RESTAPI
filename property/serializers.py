558a076c08cf2d883263af29f170c93112042fe3
edsonibarra <edsonibarra97@gmail.com>
2025-10-19 17:13:00 -0600

    refactor

--- property/serializers.py
+++ property/serializers.py
@@ -1,8 +1,101 @@
 # serializers.py
 from rest_framework import serializers
 from django.contrib.contenttypes.models import ContentType
-from .models import HouseForSale, HouseForRent, PropertyImage
 
+from owner.serializers import BaseOwnerSerializer
+from .models import BaseProperty, HouseForSale, HouseForRent, PropertyImage
+
+
+class ApartmentForRentSerializer(serializers.ModelSerializer):
+    propietario = BaseOwnerSerializer()
+    class Meta:
+        model = BaseProperty
+        fields = [
+            # Base
+            "tipo_propiedad",
+            "titulo",
+            "calle",
+            "colonia",
+            "numero",
+            "codigo_postal",
+            "ciudad",
+            "operacion",
+            "costo",
+            "propietario",
+            "estatus",
+            "caracteristicas_extras",
+            "comentarios",
+            # Especificas del apartamento en renta
+            "tiene_cochera",
+            "banos",
+            "acepta_mascotas",
+            "tiene_patio",
+            "recamaras",
+            "centro_de_lavado",
+            "minisplits",
+            "servicios_incluidos",
+            "numero_servicio_simas",
+            "numero_servicio_cfe"
+        ]
+
+
+class HouseForRentSerializer(serializers.Serializer):
+    propietario = BaseOwnerSerializer()
+    class Meta:
+        model = BaseProperty
+        fields = [
+            # Base
+            "tipo_propiedad",
+            "titulo",
+            "calle",
+            "colonia",
+            "numero",
+            "codigo_postal",
+            "ciudad",
+            "operacion",
+            "costo",
+            "propietario",
+            "estatus",
+            "caracteristicas_extras",
+            "comentarios",
+            # Especificas del casa en renta
+            "tiene_cochera",
+            "banos",
+            "acepta_mascotas",
+            "tiene_patio",
+            "recamaras",
+            "centro_de_lavado",
+            "minisplits",
+            "servicios_incluidos",
+        ]
+
+
+class HouseForRentSerializer(serializers.Serializer):
+    propietario = BaseOwnerSerializer()
+    class Meta:
+        model = BaseProperty
+        fields = [
+            # Base
+            "tipo_propiedad",
+            "titulo",
+            "calle",
+            "colonia",
+            "numero",
+            "codigo_postal",
+            "ciudad",
+            "operacion",
+            "costo",
+            "propietario",
+            "estatus",
+            "caracteristicas_extras",
+            "comentarios",
+            # Especificas del casa en venta
+            "superficie_terreno",
+            "superficie_construccion",
+            "metodo_pago",
+            "observaciones",
+            "negociable",
+        ]
 
 class PropertyImageUploadSerializer(serializers.ModelSerializer):
     content_type = serializers.CharField()

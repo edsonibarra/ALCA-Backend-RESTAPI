e3492e7ceef5dfd91ab29392aa89364dec8f6c58
edsonibarra <edsonibarra97@gmail.com>
2025-10-19 17:01:27 -0600

    se agregaron constants y choices para el nuevo modelo property

---
+++ property/property_choices.py
@@ -0,0 +1,38 @@
+CITY_CHOICES = [
+    ('monclova', 'monclova'),
+    ('frontera', 'frontera'),
+    ('castanos', 'castanos'),
+    ('sanbuena', 'sanbuena'),
+    ('saltillo', 'saltillo'),
+    ('cuatrocienegas', 'cuatrocienegas'),
+]
+
+OPERATION_CHOICES = [
+    ('venta', 'venta'),
+    ('renta', 'renta')
+]
+
+STATUS_CHOICES = [
+    ('disponible', 'disponible'),
+    ('rentada', 'rentada'),
+    ('vendida', 'vendida')
+]
+
+PROPERTY_TYPE = [
+    ('casa', 'casa'),
+    ('departamento', 'departamento'),
+    ('local comercial', 'local_comercial'),
+    ('terreno residencial', 'terreno residencial'),
+    ('terreno rustico', 'terreno rustico'),
+    ('terreno industrial', 'terreno industrial'),
+    ('nave industrial', 'nave industrial'),
+    ('bodega', 'bodega')
+]
+
+PAYMENT_METHOD = [
+    ('cualquier credito', 'cualquier credito'),
+    ('infonavit', 'infonavit'),
+    ('bancario', 'bancario'),
+    ('recursos propios', 'recursos propios')
+]
+

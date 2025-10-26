76e9412a97f60aec21ea78ad74408e0c0878bbab
edsonibarra <edsonibarra97@gmail.com>
2025-10-26 16:42:51 -0600

    use the onwer_baseowner model in the views

--- owner/views.py
+++ owner/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets, serializers
 from rest_framework.pagination import PageNumberPagination
-
-from owner.models import Owner
-
-# Create your views here.
-# class HouseForSaleViewSet(viewsets.ModelViewSet):
-#     queryset = HouseForSale.objects.all()
-#     serializer_class = HouseForSaleSerializer
+from owner.models import BaseOwner, Owner
 
 
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Owner
+        model = BaseOwner
         fields = "__all__"
 
+
 class OwnerPagination(PageNumberPagination):
-    page_size = 1000  # Permitir hasta 1000 owners por página
+    page_size = 10  # Permitir hasta 1000 owners por página
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 10
+
 
 class OwnerViewSet(viewsets.ModelViewSet):
-    queryset = Owner.objects.all().order_by("name")
+    queryset = BaseOwner.objects.all().order_by("name")
     serializer_class = OwnerSerializer
     pagination_class = OwnerPagination
